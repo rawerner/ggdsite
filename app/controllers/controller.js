46ff2ab0abad981a8777f69a024d2574227e7bba
schmitz1193 <schmitz1193@gmail.com>
2016-05-11 14:08:06 -0500

    all event titles displayed

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,6 +1,6 @@
 app.controller("ggdCtrl", ["$scope", "$http",
                             function($scope, $http) {
-
+//user datat - test call
 // var url = "https://www.eventbriteapi.com/v3/users/me/?token=RMM3VY3TF2WHZB76TNSF";
 // $http.get(url).then(function(response) {
 //   console.log("response", response.data);
@@ -8,10 +8,12 @@ app.controller("ggdCtrl", ["$scope", "$http",
 
 var url = "https://www.eventbriteapi.com/v3/users/me/owned_events/?token=RMM3VY3TF2WHZB76TNSF";
 $http.get(url).then(function(response) {
-  console.log("response", response.data);
-$scope.eventText = response.data.events[0].name.text;
-console.log("scope ", $scope.eventText);
+  console.log("response", response.data.events);
+$scope.dataEvents = response.data.events;
+$scope.dataEventFirst = response.data.events[0].name.text;
+console.log("scope ", $scope.dataEvents);
 });
+
 // function getLiveEvents() {
 //     return $http.get(url, {
 //         cache: true,
